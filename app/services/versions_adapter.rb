4d5d395525c0f19e3db5fcb34c6f0d65c0fc0364
David Ang <davidang09@gmail.com>
2015-11-18 04:21:42 +0800

    should use whodunnit to pull out user info

--- app/services/versions_adapter.rb
+++ app/services/versions_adapter.rb
@@ -7,7 +7,7 @@ class VersionsAdapter
         versions = element.versions.order(:object, created_at: :asc)
         versions.each do |version|
           prototype = version.object.nil? ? element.class.new : YamlAdapter.create(version.object, element.class)
-          prototype.who = version.originator.to_i
+          prototype.who = version.whodunnit
           prototype.version_id = version.id
           prototype.version_event = version.event
           prototype.version_changes = version.object_changes
