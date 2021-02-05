f7dd27c235ea7689ad7c25a5a7ba240b33316349
Edmundo Perez <edmundo@michelada.io>
2021-02-04 19:15:51 -0600

    adds set_paper_trail_whodunnit to base controller

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,4 +1,6 @@
 Spree::BaseController.class_eval do
+  before_action :set_paper_trail_whodunnit
+
   def user_for_paper_trail
     current_spree_user.nil? ? 'Public User' : current_spree_user.id
   end
